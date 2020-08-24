8b94b6809dbf361834f8b405d49f5e413d3a2c8e
Axel Lopez <lopezaxel@protonmail.com>
2020-08-24 09:30:54 -0300

    Create Flight model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_120058) do
+ActiveRecord::Schema.define(version: 2020_08_24_121201) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.integer "from_airport_id"
+    t.integer "to_airport_id"
+    t.datetime "date"
+    t.integer "duration"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
